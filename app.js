6579cbb295e13be3e136d01bf2b32c1ab882b051
AbhinavGor <56104674+AbhinavGor@users.noreply.github.com>
2020-06-18 02:53:19 +0530

    Configured for heroku deploy

--- app.js
+++ app.js
@@ -61,4 +61,14 @@ app.use('/users', require('./routes/users.js'));
 
 const PORT = process.env.PORT || 3000;
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
