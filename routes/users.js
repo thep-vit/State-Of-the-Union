0dca71bc44483ceb67db42ea2d69b8b419304513
AbhinavGor <56104674+AbhinavGor@users.noreply.github.com>
2020-06-19 21:05:20 +0530

    Registration fixed

--- routes/users.js
+++ routes/users.js
@@ -56,6 +56,28 @@ router.post('/register', (req, res) => {
           password,
           submitted
         });
+        req.flash(
+          'success_msg',
+          'You are now registered and can log in'
+        );
+        res.redirect('/users/login');
+
+        // bcrypt.genSalt(10, (err, salt) => {
+        //   bcrypt.hash(newUser.password, salt, (err, hash) => {
+        //     if (err) throw err;
+        //     newUser.password = hash;
+        //     newUser
+        //       .save()
+        //       .then(user => {
+        //         req.flash(
+        //           'success_msg',
+        //           'You are now registered and can log in'
+        //         );
+        //         res.redirect('/users/login');
+        //       })
+        //       .catch(err => console.log(err));
+        //   });
+        // });
       }
     });
   }
