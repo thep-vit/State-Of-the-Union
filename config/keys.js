c13bac63198aca44f54c0bc0a8f974197ed20492
AbhinavGor <56104674+AbhinavGor@users.noreply.github.com>
2020-09-01 11:05:11 +0530

    Added production db

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb://Abhinav123:Abhinav123@participant-data-shard-00-00-wavyx.mongodb.net:27017,participant-data-shard-00-01-wavyx.mongodb.net:27017,participant-data-shard-00-02-wavyx.mongodb.net:27017/details?ssl=true&replicaSet=Participant-Data-shard-0&authSource=admin&retryWrites=true&w=majority';
+dbPassword = 'mongodb+srv://Abhinav123:Abhinav123@participant-data-ce.2z40r.mongodb.net/Participants?retryWrites=true&w=majority';
 // dbPassword = 'mongodb://127.0.0.1/sou'
 
 module.exports = {
